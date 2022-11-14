1d98c0287e165fc26d3be03277436399b531adce
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2022-11-14 21:24:09 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
 
 import App from './App'
 
